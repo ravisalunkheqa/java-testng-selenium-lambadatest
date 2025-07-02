3836c95db06865c50b1f8b61246805facf1c6bab
ravisalunkheqa <151988249+ravisalunkheqa@users.noreply.github.com>
2025-07-02 15:34:51 +0530

    Update TestNGTodo3.java

--- src/test/java/com/lambdatest/TestNGTodo3.java
+++ src/test/java/com/lambdatest/TestNGTodo3.java
@@ -22,7 +22,11 @@ public class TestNGTodo3 {
     public void setup(Method m, ITestContext ctx) throws MalformedURLException {
         //String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
         //String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
-        
+
+
+        String username = "ravisalunkhe";
+        String authkey = "LT_ACCESS_KEY="LT_1zM6wZiQklihgDDdoki0af9MfWh6RERvlhplx64mlxEWTnr";
+
         String hub = "@hub.lambdatest.com/wd/hub";
 
         DesiredCapabilities caps = new DesiredCapabilities();
